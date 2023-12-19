9a57b366c43df4c9abb31365cc9c35e782684c64
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2023-12-19 01:19:29 -0600

    .

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ window.sendPrivateChatMessage = function sendPrivateChatMessage(privateMsg, priv
   console.log("Button: " + sendButton)
 
   setTimeout(() => { document.getElementById(String(sendButton)).hidden = false; }, 5000);
+  updatePrivateChats();
+  setTimeout(() => { document.getElementById(String(privateChatName)).hidden = false; }, 5000);
 }
 
 window.randomID = function randomID(length) {
