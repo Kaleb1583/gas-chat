193f3c5dc113f73200372f0202a1314c666b61c2
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2023-12-18 23:14:32 -0600

    .

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ window.createPrivateChatElement = function createPrivateChatElement(chatName, da
     document.getElementById(String(chatName)).appendChild(chatLine);
   }
   var inputbutton = document.createElement("div");
-  var msgInputId = "privateMsgInput";
-  inputbutton.innerHTML = "<br> <input id='privateMsgInput'></input> <button id='sendB' onclick='sendPrivateChatMessage(document.getElementById(String(msgInputId)).value, chatName)'>Send Private Chat</button>";
+  inputbutton.innerHTML = "<br> <input id='privateMsgInput'></input> <button id='sendB' onclick='sendPrivateChatMessage(document.getElementById(`privateMsgInput`).value, chatName)'>Send Private Chat</button>";
   document.getElementById(String(chatName)).appendChild(inputbutton);
 }
 
