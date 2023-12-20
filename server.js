2dca1148e5270fd1d8c67e5548a13483be175030
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2023-12-20 17:29:47 -0600

    .

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const http = require('http');
 
-function requestListener(req, res) {
+window.requestListener = function requestListener(req, res) {
   let forwarded = req.headers['x-forwarded-for']
   let ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
   res.writeHead(200);
   res.end(ip);
 }
 
-const server = http.createServer(requestListener);
-server.listen(3000);
-
-console.log('Server listening at http://localhost:3000');
+console.log("-")
